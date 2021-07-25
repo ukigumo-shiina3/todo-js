1c1b01e3ae1d05d46bec71d2caf7753e4f5ac9a9
daisuke33 <67767613+ukigumo-shiina3@users.noreply.github.com>
2021-07-25 22:30:45 +0900

    ボタンクリック時のアラート追加

--- src/index.js
+++ src/index.js
@@ -13,8 +13,25 @@ const onclickAdd = () => {
   const li = document.createElement("li");
   li.innerText = inputText;
 
+  // button(完了)タグ生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    alert("完了");
+  });
+
+  // button(削除)タグ生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    alert("削除");
+  });
+  console.log(deleteButton);
+
   // divタグの子要素に各要素を設定
   div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
 
   // 未完了リストに追加
   document.getElementById("incomplete_list").appendChild(div);
